b11f7393c32bc092c6b8ce7e69755d578ab5f06e
Som Ramnani <som_ramnani@Soms-MBP.home>
2021-04-29 11:20:06 -0400

    Edits TheirMessage

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,18 +1,30 @@
 import { Avatar } from "react-chat-engine";
 
-const TheirMessage = () =>{
+const TheirMessage = (lastMessage,message) =>{
   const isFirstMessageByUser= !lastMessage || lastMessage.sender.username !== message.sender.username; 
   return (
     <div className= "message-row">
       {isFirstMessageByUser && (
        <div 
         className="message-avatar"
-        style={{backgroundImage:`url(${message/sender?.avatar})`}}
-        >
+        style={{backgroundImage:`url(${message.sender?.avatar})`}}
+        > 
        </div>
        
       )}
-      TheirMessage
+      {message?.attachments?.length>0 
+        ? (<img 
+          src = {message.attachments[0].file}
+          alt= "message-attachment"
+          className="message-image"
+          style={{float:'right'}}
+          />
+        ) : (
+          <div className = "message" style={{float:'right',marginRight:'18px', color:'white', backgroundColor: '#3B2A50'} }> 
+            {message.text}
+          </div>
+        )
+      }
     </div>
   )
 }
